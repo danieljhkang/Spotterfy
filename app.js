acd81bbe0efb9ea197853acaeced4f212da6cd3c
Daniel Kang <dkang.jh@gmail.com>
2022-12-01 19:58:26 -0500

    added app.js basics

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-//Here is where you'll set up your server
+const express = require("express");
+const app = express();
+const configRoutes = require("./routes");
+const exphbs = require("express-handlebars");
+const session = require("express-session");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.engine("handlebars", exphbs.engine({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+configRoutes(app);
+
+app.listen(3000, () => {
+  console.log("We've now got a server!");
+  console.log("Your routes will be running on http://localhost:3000");
+});
