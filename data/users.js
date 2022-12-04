02dfcbbfe0411f677ac82bdf59b2c180a847d7d8
rlee12 <rlee12@stevens.edu>
2022-12-04 11:34:14 -0500

    completed check user and check user authentication in data.js

--- data/users.js
+++ data/users.js
@@ -7,10 +7,53 @@ const bcrypt = require("bcryptjs");
 const saltRounds = 10;
 
 //creates a user and adds it to the mongo database
-const createUser = async (username, password) => {};
+const createUser = async (username, password) => {
+  // input checking for username and password
+  const validUser = await helpers.checkUserInfo(username, password);
+
+  // check if username already exists (username should be case-insensitive)
+  const userCollection = await users();
+  username = username.toLowerCase();
+  let found = await userCollection.findOne({ username: username });
+  if (found) throw "This username has already been taken";
+
+  // hash password using bcrypt
+  const hash = await bcrypt.hash(password, saltRounds);
+
+  let newUser = { username: username, password: hash };
+
+  const insertUser = await userCollection.insertOne(newUser);
+  if (!insertUser.acknowledged || !insertUser.insertedId) {
+    throw "Could not create user";
+  }
+
+  return { insertedUser: true };
+};
 
 //checks to see if the user is currently authenticated
-const checkUserAuth = async (username, password) => {};
+const checkUserAuth = async (username, password) => {
+  // validate inputs
+  const validUser = await helpers.checkUserInfo(username, password);
+
+  // find user
+  const userCollection = await users();
+  username = username.toLowerCase();
+  let exist = await userCollection.findOne({ username: username });
+  if (!exist) throw "Either the username or password is invalid";
+
+  // authenticate user
+  let compare = false;
+
+  try {
+    compare = await bcrypt.compare(password, exist.password);
+  } catch (e) {
+    // no op
+  }
+
+  if (!compare) throw "Either the username or password is invalid";
+
+  return { authenticatedUser: true };
+};
 
 module.exports = {
   createUser,
