691b8bf2d90e11aef0e43bc381dbf5545299d8ff
rlee12 <rlee12@stevens.edu>
2022-12-05 12:02:44 -0500

    Implemented input checking for username, password, email and CWID

--- helpers.js
+++ helpers.js
@@ -66,42 +66,40 @@ let special = /[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
 
 //Checks if user password is existant, contains 6+ chars, and includes at least one special char/num/uppercase
 let checkUserPassword = (password) => {
-  if (!password)
-    throw "You must supply a password!";
+  if (!password) throw "You must supply a password!";
 
-    if (typeof password !== "string") 
-      throw "Username must be a string";
+  if (typeof password !== "string") throw "Username must be a string";
 
-    password = password.trim();
-    if (password === "") {
-      throw "Password is empty spaces";
+  password = password.trim();
+  if (password === "") {
+    throw "Password is empty spaces";
+  }
+  if (password.length < 6) {
+    throw "Password must be at least 6 characters";
+  }
+  if (password.includes(" ")) {
+    throw "Password can't contain empty spaces";
+  }
+
+  let oneUpper = false;
+  let oneNum = false;
+  let oneSpecial = false;
+
+  for (let i = 0; i < password.length; i++) {
+    //check for password conditions
+    if (upperLetters.includes(password[i])) {
+      oneUpper = true;
     }
-    if (password.length < 6) {
-      throw "Password must be at least 6 characters";
+    if (numbers.includes(password[i])) {
+      oneNum = true;
     }
-    if (password.includes(" ")) {
-      throw "Password can't contain empty spaces";
-    }
-  
-    let oneUpper = false;
-    let oneNum = false;
-    let oneSpecial = false;
-  
-    for (let i = 0; i < password.length; i++) {
-      //check for password conditions
-      if (upperLetters.includes(password[i])) {
-        oneUpper = true;
-      }
-      if (numbers.includes(password[i])) {
-        oneNum = true;
-      }
-    }
-  
-    oneSpecial = special.test(password);
-  
-    if (!oneUpper || !oneNum || !oneSpecial) {
-      throw "Password must have one uppercase letter, one number, and one special character";
-    }
+  }
+
+  oneSpecial = special.test(password);
+
+  if (!oneUpper || !oneNum || !oneSpecial) {
+    throw "Password must have one uppercase letter, one number, and one special character";
+  }
 };
 
 //throws an error if the given input is not valid.
@@ -130,6 +128,75 @@ const checkUserInfo = async (input) => {
       throw "input must only contain alphanumeric characters";
     }
   }
+};
+
+/* CHECK USER INFO */
+// check username
+const validUsername = async (username) => {
+  // username must be supplied
+  if (!username) throw "Please provide a username";
+  // username should be a valid string (no empty spaces, no spaces in username and only alphanumeric characters) and at least 4 characters long
+  if (typeof username !== "string" || username.trim().length === 0)
+    throw "Username must be a non-empty string";
+  username = username.trim();
+  if (username.length < 4 || !/^[A-Za-z0-9]*$/.test(username))
+    throw "Username must be alphanumeric and at least 4 characters";
+};
+
+// check email
+const validEmail = async (email) => {
+  // email must be supplied
+  if (!email) throw "Please provide email";
+  // email should be a valid string (no empty spaces, no spaces in email and only alphanumeric characters besides "@" and ".")
+  if (typeof email !== "string" || email.trim().length === 0)
+    throw "Email must be a non-empty string";
+  // split email at "@" to check for username and stevens email
+  const splitEmail = email.split("@");
+  if (splitEmail.length !== 2) throw "Please provide a valid Stevens email";
+  const username = splitEmail[0].toLowerCase;
+  const stevensEmail = splitEmail[1].toLowerCase;
+  try {
+    const validUN = await validUsername(username);
+  } catch (e) {
+    throw "Please provide a valid Stevens email";
+  }
+  if (stevensEmail !== "stevens.edu")
+    throw "Please provide a valid Stevens email";
+};
+
+// check password
+const validPW = async (password) => {
+  // password must be supplied
+  if (!password) throw "Please provide a password";
+  // password must be a valid string (no empty spaces, no spaces but can be any other character including special characters) and at least 6 characters long
+  if (
+    typeof password !== "string" ||
+    password.trim().length === 0 ||
+    /[ ]/.test(password)
+  )
+    throw "Password must be a non-empty string with no spaces";
+  password = password.trim();
+  if (password.length < 6) throw "Password must be at least 6 characters long";
+  if (!/[A-Z]/.test(password))
+    throw "Password must have at least one uppercase letter";
+  if (!/[a-z]/.test(password))
+    throw "Password must have at least one lowercase letter";
+  if (!/[0-9]/.test(password)) throw "Password must have at least one number";
+  if (!/[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/.test(password))
+    throw "Password must have at least one special character";
+};
+
+// check cwid
+const validCWID = async (cwid) => {
+  // cwid must be supplied
+  if (!cwid) throw "Please provide CWID";
+  // cwid should be a valid string (no empty spaces, no spaces in cwid, and only numbers) and only 8 characters long
+  if (
+    typeof cwid !== "string" ||
+    cwid.trim().length !== 8 ||
+    !/^[0-9]*$/.test(cwid.trim())
+  )
+    throw "CWID must be 8 digits.";
 };
 
 module.exports = {
