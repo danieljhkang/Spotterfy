5d6a9023070c611ce6b0d2e628fa335619f46f3a
Ken Yun <yunhaen@gmail.com>
2022-12-18 04:56:55 -0500

    Removed client side form validation for email field

--- public/js/register-form.js
+++ public/js/register-form.js
@@ -1,18 +1,8 @@
 (function () {
     const form = document.getElementById("register-form");
     if (form) {
-        const email = document.getElementById("emailInput");
         const cwid = document.getElementById("cwidInput");
 
-        email.addEventListener('focusout', (event) => {
-            if (email.validity.patternMismatch) {
-                email.setCustomValidity("Please enter a valid Stevens email address");
-                email.reportValidity();
-            } else {
-                email.style.background = '';
-                email.setCustomValidity("");
-            }
-        });
         cwid.addEventListener("input", (event) => {
             if (cwid.validity.patternMismatch) {
                 cwid.setCustomValidity("CWID must only contain digits");
